b5bb23fd17e4b5f530147bc890681ee165e6c808
BIAGIO AZZARELLI <b.azzarelli@gmail.com>
2024-08-14 21:04:15 -0700

    Added waynemiyata site contents

---
+++ src/components/custom/Header.jsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+function Header() {
+  return (
+    <div className="w-full relative">
+      <img
+        src="/images/top-banner.gif"
+        alt="Wayne Miyata Surfboards"
+        width={1152}
+        height={172}
+      />
+      <Link href="/">
+        <button
+          className="absolute -bottom-4 right-[11.5rem] border border-white font-sans
+        p-1 px-3 hover:-bottom-[1.1rem] hover:right-[11.4rem] hover:bg-black hover:text-white
+      z-10 flex items-center justify-center bg-brand-red text-white transition-all duration-300 ease-in-out"
+        >
+          HOME
+        </button>
+      </Link>
+      <Link href="/store">
+        <button
+          className="absolute -bottom-4 right-20 border border-white font-sans
+        p-1 px-3 hover:-bottom-[1.1rem] hover:right-[4.9rem] hover:bg-black hover:text-white
+      z-10 flex items-center justify-center bg-brand-red text-white transition-all duration-300 ease-in-out"
+        >
+          STORE
+        </button>
+      </Link>
+    </div>
+  );
+}
+export default Header;
---
+++ src/components/custom/WayneBio.jsx
@@ -0,0 +1,31 @@
+import WayneHeadshot from "../../public/images/wayne-headshot.jpg";
+
+function WayneBio() {
+  return (
+    <section className="mx-6 font-sans border-2 border-brand-red">
+      <div className="bg-brand-yellow pb-16">
+        <h3 className="text-2xl font-light bg-brand-red py-2 px-4 text-white">
+          Wayne Miyata (1942-2005)
+        </h3>
+        <img
+          src={WayneHeadshot}
+          alt="Wayne Miyata"
+          className="float-right w-48 md:w-64 mx-8 md:mb-8 md:mt-8 rounded"
+        />
+
+        <p className="pt-6 p-4 text-lg font-light leading-snug">
+          Wayne Miyata surfboards are designed and produced by real surfers that
+          know the mechanics of a high performance board. The founder, Wayne
+          Miyata, dedicated 50 years of his life to riding, designing, and
+          building surfboards.
+        </p>
+        <p className="p-4 pt-0 text-lg font-light leading-snug">
+          He perfected old-style techniques of pinlining and glossing surfboards
+          while passing down this knowledge and skill to many of the young men
+          he mentored and who continue to develop the craft as he trained them.
+        </p>
+      </div>
+    </section>
+  );
+}
+export default WayneBio;
